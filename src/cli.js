1699ac5eaa9b8dea0e50ab57fe2391082d51deb9
gopal <gopal@student.21-school.ru>
2024-06-12 19:16:16 +0300

    feat: add cli two arguments

--- src/cli.js
+++ src/cli.js
@@ -3,14 +3,17 @@ import { program } from "commander";
 export default function cli() {
   program
     .name("gendiff")
-    // .description("Compares two configuration files and shows a difference.")
-    // .option("-f, --format <type>", "output format")
-    .option("-h, --help", "Compares two configuration files and shows a difference.")
+    .description("Compares two configuration files and shows a difference.")
+    .argument("<filepath1>")
+    .argument("<filepath2>")
+    .option("-f, --format [type]", "output format")
     .version("1.0.0");
 
   program.parse();
 
-  if (program.help) {
-    program.outputHelp();
-  }
+  // const filePath1 = program.args[0];
+  // const filePath2 = program.args[1];
+  // const options = program.opts();
+
+  // console.log(`Console: format - {${options.format}}  ${filePath1} ${filePath2});`);
 }
